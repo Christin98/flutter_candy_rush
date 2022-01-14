43ded411111a3c9490aca3d9a2c804d428cf7d9b
Christin B Koshy <christinkoshy1998@gmail.com>
2022-01-14 19:48:57 +0530

    added double_curved_container.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_candy_rush/animations/shine_effect.dart';
 import 'package:flutter_candy_rush/bloc/bloc_provider.dart';
 import 'package:flutter_candy_rush/bloc/game_bloc.dart';
+import 'package:flutter_candy_rush/game_widgets/double_curved_container.dart';
 import 'package:flutter_candy_rush/game_widgets/game_level_button.dart';
 import 'package:flutter_candy_rush/game_widgets/shadowed_text.dart';
 
@@ -106,6 +108,38 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
                           },
                         );
                       }),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 0.0,
+              top: _animation.value * 250.0 - 150.0,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 30.0
+                ),
+                child: DoubleCurvedContainer(
+                  width: screenSize.width - 60.0,
+                  height: 150.0,
+                  outerColor: Colors.blue.shade700,
+                  innerColor: Colors.blue,
+                  child: Stack(
+                    children: const <Widget>[
+                      ShineEffect(
+                        offset: Offset(100.0, 100.0),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: ShadowedText(
+                          text: 'Flutter Rush',
+                          color: Colors.white,
+                          fontSize: 26.0,
+                          shadowOpacity: 1.0,
+                          offset: Offset(1.0, 1.0),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             )
